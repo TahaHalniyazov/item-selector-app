d5c616f260e03d1cb6e6de0db42343b6bd811d59
unknown <halniyazovt98@gmail.com>
2025-05-09 12:16:09 +0500

    реализовал страницу выбора предметов и стили по макету

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="area blocks-top">
+      <TopLeft :items="selectedLeft" />
+      <TopRight :item="selectedRight" />
+    </div>
+    <div class="area blocks-bottom">
+      <LeftItems
+        :items="leftItems"
+        :selected="selectedLeft"
+        @toggle="toggleLeft"
+      />
+      <RightItems
+        :items="rightItems"
+        :selected="selectedRight"
+        @select="selectRight"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import LeftItems from './components/LeftItems.vue'
+import RightItems from './components/RightItems.vue'
+import TopLeft from './components/TopLeft.vue'
+import TopRight from './components/TopRight.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { LeftItems, RightItems, TopLeft, TopRight },
+  setup () {
+    const leftItems = [
+      { id: 1, name: 'Shoes 1' },
+      { id: 2, name: 'Shoes 2' },
+      { id: 3, name: 'Shoes 3' },
+      { id: 4, name: 'Shoes 4' },
+      { id: 5, name: 'T-shirt 1' },
+      { id: 6, name: 'T-shirt 2' },
+      { id: 7, name: 'T-shirt 3' },
+      { id: 8, name: 'T-shirt 4' }
+    ]
+    const rightItems = [
+      { id: 11, name: 'Jacket 1' },
+      { id: 12, name: 'Jacket 2' },
+      { id: 13, name: 'Jacket 3' },
+      { id: 14, name: 'Jacket 4' },
+      { id: 15, name: 'Hoodie 1' },
+      { id: 16, name: 'Hoodie 2' },
+      { id: 17, name: 'Hoodie 3' },
+      { id: 18, name: 'Hoodie 4' }
+    ]
+
+    const selectedLeft = ref([])
+    const selectedRight = ref(null)
+
+    function toggleLeft (item) {
+      const idx = selectedLeft.value.findIndex(i => i.id === item.id)
+      if (idx !== -1) {
+        selectedLeft.value.splice(idx, 1)
+      } else if (selectedLeft.value.length < 6) {
+        selectedLeft.value.push(item)
+      }
+    }
+
+    function selectRight (item) {
+      if (selectedRight.value && selectedRight.value.id === item.id) {
+        selectedRight.value = null
+      } else {
+        selectedRight.value = item
+      }
+    }
+
+    return {
+      leftItems,
+      rightItems,
+      selectedLeft,
+      selectedRight,
+      toggleLeft,
+      selectRight
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LeftItems.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="block bottom-left">
+      <h3>Your Items</h3>
+      <div
+        v-for="it in items"
+        :key="it.id"
+        :class="['item', { selected: selected.some(s => s.id === it.id) }]"
+        @click="toggle(it)"
+      >
+        {{ it.name }}
+      </div>
+      <div class="info">selected: {{ selected.length }} / 6</div>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'LeftItems',
+  props: {
+    items: Array,
+    selected: Array
+  },
+  emits: ['toggle'],
+  methods: {
+    toggle (item) {
+      this.$emit('toggle', item)
+    }
+  }
+}
+</script>
